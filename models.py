bc95c63c6bdb17d1b67a3c4f1c47cc7affe3cb3d
Joe Michelini <josephmichelini@gmail.com>
2020-09-11 07:27:19 -0400

    add initial db models

--- models.py
+++ models.py
@@ -1,1 +1,32 @@
+import datetime
+
+from flask_login import UserMixin
 from peewee import *
+
+db = SqliteDatabase('journal.db')
+
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+
+    class Meta:
+        database = db
+
+
+class Entry(Model):
+    title = CharField()
+    date = DateTimeField(default=datetime.datetime.now)
+    time_spent = IntegerField()
+    learned = TextField()
+    resources = TextField(default='')
+
+    class Meta:
+        database = db
+
+
+def initialize():
+    db.connect()
+    db.create_tables([User, Entry], safe=True)
+    db.close()
